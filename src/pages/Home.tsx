18430b3210f88344c1a91dba5e35b9be62f8b9f0
Julio Brazao <jotabrazao@gmail.com>
2024-07-06 16:23:04 -0400

    Basic demonstration of UI elements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,15 @@
-import { ContainerCentered } from "../components/Containers";
+import { ContainerCentered, ContainerFluid } from "../components/Containers";
 
 export default function Home() {
-  return <ContainerCentered>This is home!</ContainerCentered>;
+  return (
+    <ContainerCentered>
+      <ContainerFluid>
+        <span className="text-primary my-3">This is basic text UI!</span>
+        <input type="text" className="form-control rounded-pill my-3" />
+        <button className="btn btn-primary rounded-pill w-100 my-3">
+          Basic Button
+        </button>
+      </ContainerFluid>
+    </ContainerCentered>
+  );
 }
