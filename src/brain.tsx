85f9566e7b09326bee28e1db5491bc47ba3a6e94
Julio Brazao <jotabrazao@gmail.com>
2024-07-06 15:59:58 -0400

    Bootstrap basic setup on basic load

--- src/brain.tsx
+++ src/brain.tsx
@@ -1,3 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import { Routes } from "./Routes";
