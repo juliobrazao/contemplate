d9b4119439d8c600aada296ed485af885af636ec
Julio Brazao <jotabrazao@gmail.com>
2024-07-06 16:56:04 -0400

    Basic UI defined

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,20 @@ export default function Home() {
   return (
     <ContainerCentered>
       <ContainerFluid>
-        <span className="text-primary my-3">This is basic text UI!</span>
+        <span className="text-primary my-3">
+          <strong>This is basic text UI!</strong>
+        </span>
         <input type="text" className="form-control rounded-pill my-3" />
         <button className="btn btn-primary rounded-pill w-100 my-3">
-          Basic Button
+          <strong>Basic Button</strong>
+        </button>
+
+        <button className="btn btn-secondary rounded-pill w-100 my-3">
+          <strong>Another Button</strong>
+        </button>
+
+        <button className="btn btn-info rounded-pill w-100 my-3">
+          <strong>Another Button</strong>
         </button>
       </ContainerFluid>
     </ContainerCentered>
