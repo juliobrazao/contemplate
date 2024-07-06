8e21d36be103479d98dc95b8cc7665259a8bb166
Julio Brazao <jotabrazao@gmail.com>
2024-07-06 17:43:26 -0400

    Removing unused spinner component

--- src/brain.tsx
+++ src/brain.tsx
@@ -3,10 +3,9 @@ import "bootstrap/dist/js/bootstrap";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import { routes } from "./Routes";
-import { Spinner } from "react-bootstrap";
 
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} fallbackElement={<Spinner />} />
+  <RouterProvider router={router} />
 );
