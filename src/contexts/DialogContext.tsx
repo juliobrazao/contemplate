314be71b2c3311d96f4ba03da272d639163b73d8
Julio Brazao <jotabrazao@gmail.com>
2024-07-07 19:46:15 -0400

    Context for dialog component

---
+++ src/contexts/DialogContext.tsx
@@ -0,0 +1,64 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useState,
+} from "react";
+
+export interface DialogProps {
+  title?: string;
+  content?: ReactNode | string;
+}
+
+export interface DialogContextProps {
+  show: boolean;
+  setShow: Dispatch<SetStateAction<boolean>>;
+  currentDialogProps: DialogProps;
+  setCurrentDialogProps: Dispatch<SetStateAction<DialogProps>>;
+  handleSetAndShowDialog: (dialogProps: DialogProps) => void;
+}
+
+interface DialogContextProviderProps {
+  children: ReactNode;
+}
+
+export const DialogContext = createContext<Partial<DialogContextProps>>({
+  show: false,
+  setShow: () => {},
+  currentDialogProps: {},
+  setCurrentDialogProps: () => {},
+  handleSetAndShowDialog: () => {},
+});
+
+export default function DialogContextProvider({
+  children,
+}: DialogContextProviderProps) {
+  const [show, setShow] = useState<boolean>(true);
+  const [currentDialogProps, setCurrentDialogProps] = useState<DialogProps>({
+    title: "",
+    content: "",
+  });
+
+  const handleSetAndShowDialog = ({ title, content }: DialogProps) => {
+    setCurrentDialogProps({
+      title,
+      content,
+    });
+    setShow(!show);
+  };
+
+  return (
+    <DialogContext.Provider
+      value={{
+        show,
+        setShow,
+        currentDialogProps,
+        setCurrentDialogProps,
+        handleSetAndShowDialog,
+      }}
+    >
+      {children}
+    </DialogContext.Provider>
+  );
+}
